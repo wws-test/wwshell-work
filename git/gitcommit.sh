9bd66ecf062acb597ba19929af1bb7c05eeabf81
sam_morton <sam_morton@devide>
2023-12-01 14:32:35 +0800

    Auto Commit

---
+++ git/gitcommit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+read -p "请输入代码仓库的链接（按回车使用默认值）: " repository_url
+repository_url=${repository_url:-http://git.git}
+read -p "请输入要统计的分支名称: " branch
+total_files=0
+total_loc=0
+total_commits=0
+
+# 克隆代码仓库到临时目录
+git clone $repository_url temp_repository
+cd temp_repository
+
+# 切换到指定分支
+git checkout $branch
+
+# 获取最近30天的提交记录
+start_date=$(date -d "-30 days" +%Y-%m-%d)
+commits=$(git rev-list --since="$start_date" $branch)
+
+# 遍历每个提交，并计算受影响的文件数、代码行数和提交数量
+for commit in $commits; do
+  files=$(git diff --name-only $commit | wc -l)
+  loc=$(git diff --shortstat $commit | awk '{print $1}')
+  total_files=$((total_files + files))
+  total_loc=$((total_loc + loc))
+  total_commits=$((total_commits + 1))
+done
+
+echo "Total files affected in the last 30 days: $total_files"
+echo "Total lines of code affected in the last 30 days: $total_loc"
+echo "Total commits in the last 30 days: $total_commits"
+
+# 删除临时克隆的仓库
+cd ..
+rm -rf temp_repository
