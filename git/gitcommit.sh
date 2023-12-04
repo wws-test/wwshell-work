29dff51f8d9ef14e281dca20da6fbecfc21a08c7
sam_morton <sam_morton@devide>
2023-12-04 11:06:00 +0800

    Auto Commit

--- git/gitcommit.sh
+++ git/gitcommit.sh
@@ -1,46 +1,38 @@
 #!/bin/bash
 
-get_commit_records() {
-  local branch=$1
-  local start_date=$(date -d "-30 days" +%Y-%m-%d)
-  git rev-list --since="$start_date" --count $branch
-}
-
-get_affected_files_and_loc() {
-  local commit=$1
-  git diff --numstat $commit | awk '{ files+=$1; loc+=$1+$2 } END { print files, loc }'
-}
-
-read -p "请输入代码仓库的链接（按回车使用默认值）: " repository_url
-repository_url=${repository_url:-http://git.git}
-read -p "请输入要统计的分支名称: " branch
-
-# 克隆代码仓库到临时目录
-git clone $repository_url temp_repository
-cd temp_repository
-
-# 切换到指定分支
-git checkout $branch
-
-# 获取最近30天的提交记录
-commit_records=$(get_commit_records $branch)
-
+repositories=("http://git.git1" "http://git.git2" "http://git.git3")
+branch="ailpha/qu/dev"
 total_files=0
-total_loc=0
+total_lines_added=0
+
 total_commits=0
 
-# 遍历每个提交，并计算受影响的文件数、代码行数和提交数量
-for commit in $commit_records; do
-  read files loc < <(get_affected_files_and_loc $commit)
-  total_files=$((total_files + files))
-  total_loc=$((total_loc + loc))
-  total_commits=$((total_commits + 1))
+for repository_url in "${repositories[@]}"; do
+  # 克隆代码仓库到临时目录
+  git clone "$repository_url" temp_repository
+  cd temp_repository
+
+  # 切换到指定分支
+  git checkout "$branch"
+
+  # 获取最近30天的提交记录
+  start_date=$(date -d "-30 days" +%Y-%m-%d)
+  commits=$(git rev-list --since="$start_date" "$branch")
+
+  # 遍历每个提交，并计算受影响的文件数、新增行数、修改行数和提交数量
+  for commit in $commits; do
+    files=$(git diff --name-only "$commit" | wc -l)
+    lines_added=$(git diff --shortstat $commit | awk '{print $4}')
+    total_files=$((total_files + files))
+    total_lines_added=$((total_lines_added + lines_added))
+    total_commits=$((total_commits + 1))
+  done
+
+  # 删除临时克隆的仓库
+  cd ..
+  rm -rf temp_repository
 done
 
 echo "Total files affected in the last 30 days: $total_files"
-echo "Total lines of code affected in the last 30 days: $total_loc"
+echo "Total lines added in the last 30 days: $total_lines_added"
 echo "Total commits in the last 30 days: $total_commits"
-
-# 删除临时克隆的仓库
-cd ..
-rm -rf temp_repository
