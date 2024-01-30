e68d88244f3e40b09bde1ae897e115ae2cb59870
Luciana Denley <1092587222@qq.com>
2024-01-30 16:14:03 +0800

    注释添加

--- jacoco/jacococli.sh
+++ jacoco/jacococli.sh
@@ -9,12 +9,14 @@ if [ -z "$jar_file" ]; then
   exit 1
 fi
 
-# 执行jacoco命令
+# 执行jacoco命令生成exec文件
 java -jar /home/org.jacoco.cli-0.8.7-SNAPSHOT-nodeps.jar dump --address 127.0.0.1 --port 18513 --destfile ./jacoco.exec
 
+# 执行jacoco命令生成xml文件
 java -jar org.jacoco.cli-0.8.7-SNAPSHOT-nodeps.jar report jacoco.exec \
 --classfiles "$jar_file" \
 --sourcefiles /home/logsaas/ailpha_code/bigdata-web-backend/bdweb-mirror/src/main/java/com/dbapp \
 --xml ./report.xml   #--html ./report
 
+# 最终生成报告html
 reportgenerator "-reports:./report.xml" "-targetdir:coveragereport" -reporttypes:HtmlSummary
